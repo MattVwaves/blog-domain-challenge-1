0ca129a20e6adf5d563aa941f131833515fc7257
MattVwaves <jamesmattholt@gmail.com>
2023-02-21 09:49:20 +0000

    username made unique

---
+++ prisma/migrations/20230120104757_reviews/migration.sql
@@ -0,0 +1,31 @@
+-- DropIndex
+DROP INDEX "Comment_userId_key";
+
+-- DropIndex
+DROP INDEX "Post_userId_key";
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "name" INTEGER NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PostToTag" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PostToTag_AB_unique" ON "_PostToTag"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PostToTag_B_index" ON "_PostToTag"("B");
+
+-- AddForeignKey
+ALTER TABLE "_PostToTag" ADD FOREIGN KEY ("A") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostToTag" ADD FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,22 +1,22 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
   const createdUser = await prisma.user.create({
-    data: { username: "alicemart", email: "him@him.com" },
+    data: { username: 'alicemart', email: 'him@him.com' },
   });
 
   const createdProfile = await prisma.profile.create({
     data: {
-      picture: "alicemart",
-      bio: "this and that",
+      picture: 'alicemart',
+      bio: 'this and that',
       user: { connect: { id: createdUser.id } },
     },
   });
 
   const createdPost = await prisma.post.create({
     data: {
-      title: "what a post geeez!:):)",
+      title: 'what a post geeez!:):)',
       content: "yes it is THIS good, you're right...",
       published: true,
       user: { connect: { id: createdUser.id } },
@@ -25,14 +25,47 @@ async function seed() {
 
   const createdComment = await prisma.comment.create({
     data: {
-      content: "this is SOME post friend",
+      content: 'this is SOME post friend',
       user: { connect: { id: createdUser.id } },
       post: { connect: { id: createdPost.id } },
     },
   });
 
+  const createTag = await prisma.tag.createMany({
+    data: [
+      {
+        name: 'politics',
+      },
+      {
+        name: 'cats',
+      },
+      {
+        name: 'goodstuff',
+      },
+    ],
+  });
+
+  // const tags = await prisma.tags.findMany({});
+  // console.log("tags", tags);
+
   process.exit(0);
 }
+
+// await prisma.category.createMany({
+//   data: [
+//     {
+//       name: "news",
+//     },
+//     {
+//       name: "personal",
+//     },
+//     {
+//       name: "random",
+//     },
+//   ],
+// });
+// const categories = await prisma.category.findMany({});
+// console.log("categories", categories);
 
 seed().catch(async (error) => {
   console.error(error);
